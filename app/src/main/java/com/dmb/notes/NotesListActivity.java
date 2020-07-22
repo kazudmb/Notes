1e00b45518f5dd5ebcd5cac6400a9a28b104348f
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-22 21:22:37 +0900

    add RecyclerView OnClickListener
    https://codingwithmitch.com/courses/sqlite-room-persistence-android/recyclerview-onclicklistener/

--- app/src/main/java/com/dmb/notes/NotesListActivity.java
+++ app/src/main/java/com/dmb/notes/NotesListActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.dmb.notes.adapters.NotesRecyclerAdapter;
 import com.dmb.notes.models.Note;
@@ -12,7 +13,9 @@ import com.dmb.notes.util.VerticalSpacingItemDecorator;
 
 import java.util.ArrayList;
 
-public class NotesListActivity extends AppCompatActivity {
+public class NotesListActivity extends AppCompatActivity implements NotesRecyclerAdapter.OnNoteListener{
+
+    private static final String TAG = "NotesListActivity";
 
     // ui components
     private RecyclerView recyclerView;
@@ -50,7 +53,12 @@ public class NotesListActivity extends AppCompatActivity {
         VerticalSpacingItemDecorator verticalSpacingItemDecorator = new VerticalSpacingItemDecorator(10);
         recyclerView.addItemDecoration(verticalSpacingItemDecorator);
         recyclerView.setLayoutManager(linearLayoutManager);
-        notesRecyclerAdapter = new NotesRecyclerAdapter(notes);
+        notesRecyclerAdapter = new NotesRecyclerAdapter(notes, this);
         recyclerView.setAdapter(notesRecyclerAdapter);
+    }
+
+    @Override
+    public void onNoteClick(int position) {
+        Log.d(TAG, "onNoteClick: clicked." + position);
     }
 }
--- app/src/main/java/com/dmb/notes/adapters/NotesRecyclerAdapter.java
+++ app/src/main/java/com/dmb/notes/adapters/NotesRecyclerAdapter.java
@@ -2,6 +2,7 @@ package com.dmb.notes.adapters;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,38 +15,56 @@ import java.util.ArrayList;
 
 public class NotesRecyclerAdapter extends RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder> {
 
-    private ArrayList<Note> mNotes = new ArrayList<>();
+    private static final String TAG = "NotesRecyclerAdapter";
 
-    public NotesRecyclerAdapter(ArrayList<Note> mNotes) {
-        this.mNotes = mNotes;
+    private ArrayList<Note> notes = new ArrayList<>();
+    private OnNoteListener onNoteListener;
+
+    public NotesRecyclerAdapter(ArrayList<Note> notes, OnNoteListener onNoteListener) {
+        this.notes = notes;
+        this.onNoteListener = onNoteListener;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_note_list_item, parent, false);
-        return new ViewHolder(view);
+        return new ViewHolder(view, onNoteListener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.timestamp.setText(mNotes.get(position).getTimestampe());
-        holder.title.setText(mNotes.get(position).getTitle());
+        holder.timestamp.setText(notes.get(position).getTimestampe());
+        holder.title.setText(notes.get(position).getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return mNotes.size();
+        return notes.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView title, timestamp;
+        OnNoteListener onNoteListener;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView, OnNoteListener onNoteListener) {
             super(itemView);
             title = itemView.findViewById(R.id.note_title);
             timestamp = itemView.findViewById(R.id.note_timestamp);
+            this.onNoteListener = onNoteListener;
+
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            Log.d(TAG, "onClick: " + getAdapterPosition());
+            onNoteListener.onNoteClick(getAdapterPosition());
+        }
+    }
+
+    public interface OnNoteListener {
+        void onNoteClick(int position);
     }
 }
