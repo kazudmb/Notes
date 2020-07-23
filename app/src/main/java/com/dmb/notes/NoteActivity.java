25b74bae6a7a995526dc323fd8d802ccd20ff79a
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-23 17:24:42 +0900

    add Setting Initial Note Properties
    https://codingwithmitch.com/courses/sqlite-room-persistence-android/setting-initial-note-properties/

--- app/src/main/java/com/dmb/notes/NoteActivity.java
+++ app/src/main/java/com/dmb/notes/NoteActivity.java
@@ -20,30 +20,45 @@ public class NoteActivity extends AppCompatActivity {
 
     // vars
     private boolean isNewNote;
+    private Note noteInitial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        linedEditText = findViewById(R.id.note_text);
+        editTitle = findViewById(R.id.note_edit_title);
+        viewTitle = findViewById(R.id.note_text_title);
 
         if (getIncomingIntent()) {
             // this is a new note, (EDIT MODE)
-
+            setNewNoteProperties();
         } else {
             // this is NOT a new note, (VIEW MODE)
-
+            setNoteProperties();
         }
     }
 
     private boolean getIncomingIntent() {
         if (getIntent().hasExtra("selected_note")) {
-            Note incomingNote = getIntent().getParcelableExtra("selected_note");
-            Log.d(TAG, "onCreate: " + incomingNote.toString());
+            noteInitial = getIntent().getParcelableExtra("selected_note");
+            Log.d(TAG, "onCreate: " + noteInitial.toString());
 
             isNewNote = false;
             return false;
         }
         isNewNote = true;
         return true;
+    }
+
+    private void setNewNoteProperties() {
+        viewTitle.setText("Note Title");
+        editTitle.setText("Note Title");
+    }
+
+    private void setNoteProperties() {
+        viewTitle.setText(noteInitial.getTitle());
+        editTitle.setText(noteInitial.getTitle());
+        linedEditText.setText(noteInitial.getContent());
     }
 }
