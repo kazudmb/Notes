f10a1b1b45ea66593080aa93b561948e91d464ef
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-23 17:13:31 +0900

    add NoteActivity Initialization
    https://codingwithmitch.com/courses/sqlite-room-persistence-android/noteactivity-initialization/

--- app/src/main/java/com/dmb/notes/NoteActivity.java
+++ app/src/main/java/com/dmb/notes/NoteActivity.java
@@ -3,6 +3,8 @@ package com.dmb.notes;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.dmb.notes.models.Note;
 
@@ -11,14 +13,37 @@ public class NoteActivity extends AppCompatActivity {
 
     private static final String TAG = "NoteActivity";
 
+    // ui components
+    private LinedEditText linedEditText;
+    private EditText editTitle;
+    private TextView viewTitle;
+
+    // vars
+    private boolean isNewNote;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
-        if(getIntent().hasExtra("selected_note")){
-            Note note = getIntent().getParcelableExtra("selected_note");
-            Log.d(TAG, "onCreate: " + note.toString());
+        if (getIncomingIntent()) {
+            // this is a new note, (EDIT MODE)
+
+        } else {
+            // this is NOT a new note, (VIEW MODE)
+
         }
+    }
+
+    private boolean getIncomingIntent() {
+        if (getIntent().hasExtra("selected_note")) {
+            Note incomingNote = getIntent().getParcelableExtra("selected_note");
+            Log.d(TAG, "onCreate: " + incomingNote.toString());
+
+            isNewNote = false;
+            return false;
+        }
+        isNewNote = true;
+        return true;
     }
 }
